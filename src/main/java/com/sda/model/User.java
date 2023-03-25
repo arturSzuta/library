f7319438522d22b7c8da71739e5b6038bea3ffd6
arturSzuta <artszuta@hotmail.com>
2023-03-25 15:01:56 +0100

    user provider update

--- src/main/java/com/sda/model/User.java
+++ src/main/java/com/sda/model/User.java
@@ -1,6 +1,7 @@
 package com.sda.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 
 public class User {
     private String name;
--- src/main/java/com/sda/provider/UserFileProvider.java
+++ src/main/java/com/sda/provider/UserFileProvider.java
@@ -1,12 +1,17 @@
 package com.sda.provider;
 
+import com.sda.model.Address;
+import com.sda.model.Role;
 import com.sda.model.User;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class UserFileProvider implements UserProvider {
 
@@ -36,7 +41,32 @@ public class UserFileProvider implements UserProvider {
     }
 
     private User mapToUser(String userLine) {
+        String[] lineResult = userLine.split(";");
+        User.builder()
+                .name(lineResult[0])
+                .lastName(lineResult[1])
+                .login(lineResult[2])
+                .password(lineResult[3])
+                .address(mapToAddress(lineResult))
+                .roles(mapToRoles(lineResult))
+                .build();
+
         return null;
+    }
+
+    private List<Role> mapToRoles(String[] lineResult) {
+        return Arrays.stream(lineResult[8].split("/"))
+                .map(Role::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    private Address mapToAddress(String[] lineResult) {
+        return Address.builder()
+                .street(lineResult[4])
+                .buildingNo(lineResult[5])
+                .apartmentNo(lineResult[6])
+                .postalCode(lineResult[7])
+                .build();
     }
 
 }
